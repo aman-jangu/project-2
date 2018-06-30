e4d9101b8ebc37719b99ae9d22d44f38433d198f
aman-jangu <jangubishnoi@gmail.com>
2018-06-30 19:13:46 +0530

    add these files

---
+++ Desktop/website/js/scripts.js
@@ -0,0 +1,11 @@
+/* var a=4;
+var b=7;
+var c="this is a string";
+var myname='and my name is Aman'
+document.write(a*b+' '+c+' '+myname);
+document.write('<br>');
+document.write("javascript is working"); */
+
+/* var profile-info= document.getElementById('profile-info');
+var first= 'aman';
+profile-info.innerHTML= first; */
